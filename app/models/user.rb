3befb07cddfbabde370a6f5a4197ab6ca7785d83
KugelSchreibe <leva-07@inbox.ru>
2022-09-04 19:32:03 +0300

    fix mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true, length: { in: 3..40 },
             format: { with: /\A[\w]+\z/ }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :header_color, format: { with: /\A(#FFF[0-9a-fA-f]{0,3})|(#0edab9)|(#0EDAB9)\z/ }
+  #validates :header_color, format: { with: /\A(#FFF[0-9a-fA-f]{0,3})|(#0edab9)|(#0EDAB9)\z/ }
 
   @@DEFAULT_HEADER_COLOR = '#0BA5BE'
 
