eb72a14dcba9dfbd0c3b3cd153f85252a52b5e7f
KugelSchreibe <leva-07@inbox.ru>
2022-09-06 17:02:02 +0300

    refactoring User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  include Gravtastic
+
+  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+
+  @@DEFAULT_HEADER_COLOR = '#0BA5BE'
+
   has_many :questions, dependent: :delete_all
 
   before_validation :downcase_nickname
@@ -10,11 +16,6 @@ class User < ApplicationRecord
             format: { with: /\A[\w]+\z/ }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   #validates :header_color, format: { with: /\A(#FFF[0-9a-fA-f]{0,3})|(#0edab9)|(#0EDAB9)\z/ }
-
-  include Gravtastic
-  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
-
-  @@DEFAULT_HEADER_COLOR = '#0BA5BE'
 
   def self.DEFAULT_HEADER_COLOR
     @@DEFAULT_HEADER_COLOR
