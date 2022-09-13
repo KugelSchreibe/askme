1f98a4753b4a59cdcd371dd00c4505730c25544d
KugelSchreibe <leva-07@inbox.ru>
2022-09-13 10:51:30 +0300

    add nickname url for question page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true, length: { in: 3..40 },
             format: { with: /\A[\w]+\z/ }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :header_color, format: { with: /\A#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\z/ }
+  validates :header_color, format: { with: /\A#([\h]{3}){1,2}\z/ }
 
   has_secure_password
   gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
