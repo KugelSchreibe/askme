8dc1ba79e6aaa057b6a548c37d427aa315107bcb
KugelSchreibe <leva-07@inbox.ru>
2022-09-16 14:38:12 +0300

    fix path helper in views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,19 +3,8 @@ module UsersHelper
     "@#{nickname}"
   end
 
+
   def header_color_checking(user)
     user&.header_color || '#0BA5BE'
-  end
-
-  def user_edit_profile_path(user)
-    "/users/#{user.nickname}/edit"
-  end
-
-  def user_profile_path(user)
-    "/users/#{user.nickname}"
-  end
-
-  def user_update(user)
-    "/users/#{user.nickname}"
   end
 end
