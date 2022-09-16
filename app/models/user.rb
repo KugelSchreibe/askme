986db151bb37bc41d6525fd0e4d92664f0a0dad0
KugelSchreibe <leva-07@inbox.ru>
2022-09-16 14:04:24 +0300

    edit email validating

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
 
   before_validation :downcase_nickname
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :nickname, presence: true, uniqueness: true, length: { in: 3..40 },
             format: { with: /\A[\w]+\z/ }
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :header_color, format: { with: /\A#(\h{3}){1,2}\z/ }
 
   has_secure_password
